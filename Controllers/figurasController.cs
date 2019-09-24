9307be81395f0490a7d98a3bccd3dd40b0063b00
hector <hectorojeda995@gmail.com>
2019-09-23 23:27:49 -0500

    version hector

---
+++ Controllers/figurasController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using figuras_mvc.Models;
+
+namespace figuras_mvc.Controllers
+{
+    public class figurasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string mensaje()
+        {
+            return "hola";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
