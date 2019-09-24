a327d0118428ae2c998d80cf168d3272009bbdeb
MrFlz <flonoalemil@gmail.com>
2019-09-24 12:44:10 -0500

    Subiendo v3 de Alam

---
+++ Controllers/figurasController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using figurasmvc.Models;
+
+
+namespace figurasmvc.Controllers{
+
+    public class figurasController : Controller{
+
+        public IActionResult Area(){
+            return View();
+        }
+    }
+}
